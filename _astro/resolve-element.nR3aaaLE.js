0ffd5e07e7b691ec091ba6147441ce23a69d4c7a
Jonny Ngo Luong <shirajuki00@gmail.com>
2024-06-16 15:25:11 +0200

    Updates

--- _astro/resolve-element.nR3aaaLE.js
+++
@@ -1,1 +0,0 @@
-import{D as a}from"./motion.n9FrB57f.js";function v(r,o,i){var f;if(typeof r=="string"){let u=document;o&&(a(!!o.current),u=o.current),i?((f=i[r])!==null&&f!==void 0||(i[r]=u.querySelectorAll(r)),r=i[r]):r=u.querySelectorAll(r)}else r instanceof Element&&(r=[r]);return Array.from(r||[])}export{v as r};
